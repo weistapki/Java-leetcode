f1d76a160bbc27fd2302ac30afdb624edca77dcf
weistapki <weistapki@gmail.com>
2023-08-23 21:19:23 +0200

    correct

--- src/main/java/org/example/singleNumber/Main.java
+++ src/main/java/org/example/singleNumber/Main.java
@@ -1,4 +1,19 @@
 package org.example.singleNumber;
 
 public class Main {
+    public static void main(String[] args) {
+        int[] nums1 = {2, 2, 1};
+        int[] nums2 = {4, 1, 2, 1, 2};
+        int[] nums3 = {1};
+
+        Solution solution = new Solution();
+
+        int result1 = solution.singleNumber(nums1);
+        int result2 = solution.singleNumber(nums2);
+        int result3 = solution.singleNumber(nums3);
+
+        System.out.println("Result 1: " + result1); // Вывод: 1
+        System.out.println("Result 2: " + result2); // Вывод: 4
+        System.out.println("Result 3: " + result3); // Вывод: 1
+    }
 }
--- src/main/java/org/example/singleNumber/Solution.java
+++ src/main/java/org/example/singleNumber/Solution.java
@@ -1,4 +1,14 @@
 package org.example.singleNumber;
 
 public class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0; // Create a variable to store the unique element, initialize it to 0
+
+        for (int num : nums) { // Iterate through all elements in the array
+            result ^= num; // Perform the XOR operation between the current result and the current element num
+        }
+
+        return result; // Return the unique element that remains after the XOR operations
+    }
 }
+
